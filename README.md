fbdfef57e92dc577e468e2270b6bcf95180c79e1
Dk <danni.kass@gmail.com>
2024-12-02 16:52:16 +0200

    den kan nu vinka och säga hej

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Why are we still here? Just to suffer? Every night, I can feel my leg and my arm... even my fingers... the body I've lost... the comrades I've lost... won't stop hurting. It's like they're all still there.
 
 https://www.youtube.com/watch?v=ZtjFsQBuJWw&pp=ygUfaSBoYXZlIGEgcXVlc3Rpb24gZm9yIGdvZCB3aHl5eQ%3D%3D
+
+
+Raport:
+
+Daniel: Lagade roboten och vinkka.
---
+++ translations/translation_en_US.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Welcome to a streching routine!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Welcome to a streching routine!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Welcome/Say</name>
+        <message>
+            <source>Welcome to a streching routine!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Welcome to a streching routine!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
+</TS>
