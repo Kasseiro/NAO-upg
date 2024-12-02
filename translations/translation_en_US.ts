c1d28d8ae98482315e81ccf1b0aec7c405b050e9
Dk <danni.kass@gmail.com>
2024-12-02 19:09:12 +0200

    head yaw

--- translations/translation_en_US.ts
+++ translations/translation_en_US.ts
@@ -2,6 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
     <context>
+        <name>behavior_1/behavior.xar:/Goodbye/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>Hello</source>
@@ -12,6 +20,22 @@
             <source>Welcome to a streching routine!</source>
             <comment>Text</comment>
             <translation type="obsolete">Welcome to a streching routine!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Goodbye</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Goodbye</translation>
+        </message>
+        <message>
+            <source>Why are we still here? Just to suffer? Every night, I can feel my leg and my arm... even my fingers... the body I've lost... the comrades I've lost... won't stop hurting. It's like they're all still there.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Why are we still here? Just to suffer? Every night, I can feel my leg and my arm... even my fingers... the body I've lost... the comrades I've lost... won't stop hurting. It's like they're all still there.</translation>
+        </message>
+        <message>
+            <source>Why do we have to do this.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Why do we have to do this.</translation>
         </message>
     </context>
     <context>
